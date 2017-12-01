06691ba818dffbadeffd289b3d212b0c1fac065e
Tim Perry <pimterry@gmail.com>
2017-12-01 17:40:23 +0100

    Minimal Mockttp HTTP configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hi');
+import { getLocal } from 'mockttp';
+
+let mockServer = getLocal({
+    https: {
+        keyPath: '../mockttp/test/fixtures/test-ca.key',
+        certPath: '../mockttp/test/fixtures/test-ca.pem'
+    }
+});
+
+mockServer.start().then(() => {
+    console.log('Server started on', mockServer.port);
+
+    setTimeout(() => {
+        mockServer.stop();
+    }, 30000);
+});
