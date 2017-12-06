cf84bfa3b4f1b1002fe279a004590ca32f3b80a9
Tim Perry <pimterry@gmail.com>
2017-12-06 15:26:18 +0100

    Proxy traffic through to real servers

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
+import * as os from 'os';
+import * as path from 'path';
+
 import { getLocal } from 'mockttp';
+
+const configDir = path.join(os.homedir(), '.httptoolkit/')
 
 let mockServer = getLocal({
     https: {
-        keyPath: '../mockttp/test/fixtures/test-ca.key',
-        certPath: '../mockttp/test/fixtures/test-ca.pem'
-    }
+        keyPath: path.join(configDir, 'ca.key'),
+        certPath: path.join(configDir, 'ca.pem')
+    },
+    debug: true
 });
 
-mockServer.start().then(() => {
+mockServer.start().then(async () => {
     console.log('Server started on', mockServer.port);
 
-    setTimeout(() => {
-        mockServer.stop();
-    }, 30000);
+    await mockServer.get('/').thenReply(200, 'Running!');
+    await mockServer.anyRequest().always().thenPassThrough();
 });
