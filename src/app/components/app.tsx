6c0af4ec887053689735f542cd8766bb83fa1561
Tim Perry <pimterry@gmail.com>
2018-01-08 23:57:29 +0000

    Set up typestyle

--- src/app/components/app.tsx
+++ src/app/components/app.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { style } from 'typestyle';
 
 import { Mockttp } from 'mockttp';
 import { RequestListContainer } from './request-list-container';
@@ -7,13 +8,22 @@ import { MockttpRequest } from '../types';
 import { connect } from 'react-redux';
 import { StoreModel, ServerStatus } from '../model/store';
 
+const listClass = style({
+    fontFamily: 'Verdana',
+    width: '50%'
+});
+
 class App extends React.Component<{
     serverStatus: ServerStatus
 }> {
     render(): JSX.Element {
         switch(this.props.serverStatus) {
             case ServerStatus.Connected:
-                return <RequestListContainer></RequestListContainer>;
+                return (
+                    <div className={listClass}>
+                        <RequestListContainer />
+                    </div>
+                );
             case ServerStatus.Connecting:
                 return <div>Connecting...</div>;
             case ServerStatus.AlreadyInUse:
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import { normalize, setupPage } from 'csstips';
 
 import { AppContainer } from './components/app';
 import { getStore } from './model/store';
+
+const APP_ELEMENT_SELECTOR = "#app";
 
 window.onload = async function startApp() {
     const store = await getStore();
@@ -12,7 +15,10 @@ window.onload = async function startApp() {
         <Provider store={store}>
             <AppContainer />
         </Provider>
-    , document.getElementById('app'));
+    , document.querySelector(APP_ELEMENT_SELECTOR));
+
+    normalize();
+    setupPage(APP_ELEMENT_SELECTOR);
 }
 
 if (module.hot) {
