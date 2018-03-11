3c4e6c61247e613a99461fa71310137187c2541e
Tim Perry <pimterry@gmail.com>
2018-03-11 18:27:43 +0100

    Show a separator between split screen elements

--- src/app/components/split-screen.tsx
+++ src/app/components/split-screen.tsx
@@ -13,10 +13,31 @@ const SplitScreenElement = styled.div`
     overflow: auto;
 `;
 
+const Separator = styled.div`
+    width: 6px;
+    height: 100%;
+    position: relative;
+    background-color: #f3f4f5;
+    cursor: col-resize;
+
+    &:before {
+        content: '';
+        width: 2px;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 2px;
+        background: linear-gradient(to bottom, rgba(28,50,74,0) 0%, rgba(28,50,74,0.8) 30%, rgba(28,50,74,0.8) 70%, rgba(28,50,74,0) 100%);
+    }
+`;
+
 export const SplitScreen = (props: { children: JSX.Element[] }) => (
     <SplitScreenContainer>
         { 
-            props.children.map((c, i) => <SplitScreenElement key={i}>{ c }</SplitScreenElement>)
+            props.children
+            .map((c, i) => <SplitScreenElement key={i}>{ c }</SplitScreenElement>)
+            .reduce((result: JSX.Element[], element: JSX.Element) => result.concat(element, <Separator/>), [])
+            .slice(0, -1) // Drop the final separator
         }
     </SplitScreenContainer>
 );
