f63c3f26479b706d5c12576649f15f25a7c58351
Tim Perry <pimterry@gmail.com>
2018-03-11 01:35:16 +0100

    Add lots of table styling

--- src/app/components/request-list.tsx
+++ src/app/components/request-list.tsx
@@ -4,17 +4,49 @@ import styled, { css } from 'styled-components';
 
 import { MockttpRequest } from '../types';
 
-const FullWidthTable = styled.table`
-    display: block;
-    background-color: #eee;
+const FullSizeTable = styled.table`
+    width: 100%;
+    height: 100%;
+
+    background-color: #1c324a;
+    color: #eee;
+
+    font-size: 14px;
+
+    border-collapse: separate;
+    border-spacing: 0 3px;
+    margin-top: -3px;
 `;
 
 const CellBase = css`
     padding: 5px;
 `;
 
+const TBody = styled.tbody`
+    &:empty {
+        display: block;
+    }
+`;
+
 const Tr = styled.tr`
     ${CellBase}
+    width: 100%;
+    word-break: break-all;
+
+    /* Acts as a default height, when the table isn't yet full */
+    height: 26px;
+
+    background-color: rgba(255, 255, 255, 0.8);
+    color: #222;
+
+    &:hover {
+        background-color: rgba(255, 255, 255, 0.85);
+        cursor: pointer;
+    }
+
+    :first-child {
+        border-left: 3px solid #1c324a;
+    }
 `;
 
 const Th = styled.th`
@@ -23,23 +55,62 @@ const Th = styled.th`
 
 const Td = styled.td`
     ${CellBase}
+
+    &.method {
+        white-space: nowrap;
+        font-weight: 800;
+    }
 `;
 
+const TFoot = styled.tfoot`
+    height: auto;
+`;
+
+const Ellipsis = styled(({ className }: { className?: string }) => 
+    <span className={className}>&nbsp;…&nbsp;</span>
+)`
+    opacity: 0.5;
+`;
+
+const truncate = (str: string, length: number, trailingLength: number = 0) => {
+    if (str.length <= length) {
+        return str;
+    } else {
+        return <>
+            {str.slice(0, length - 3 - trailingLength)}
+            <Ellipsis/>
+            {str.slice(str.length - trailingLength)}
+        </>;
+    }
+}
+
+const RequestRow = ({ request }: { request: MockttpRequest }) => {
+    const url = new URL(request.url);
+
+    return <Tr>
+        <Td className='method'>{request.method}</Td>
+        <Td>{truncate(url.host, 30, 4)}</Td>
+        <Td>{truncate(url.pathname, 40, 4)}</Td>
+        <Td>{truncate(url.search.slice(1), 40)}</Td>
+    </Tr>
+}
+
 export function RequestList({ requests }: { requests: MockttpRequest[] }) {
-    return <FullWidthTable>
+    return <FullSizeTable>
         <thead>
             <Tr>
                 <Th>Method</Th>
-                <Th>URL</Th>
+                <Th>Host</Th>
+                <Th>Path</Th>
+                <Th>Params</Th>
             </Tr>
         </thead>
-        <tbody>
+        <TBody>
             { requests.map((req, i) => (
-                <Tr key={i}>
-                    <Td>{req.method}</Td>
-                    <Td>{req.url}</Td>
-                </Tr>
+                <RequestRow key={i} request={req} />
             )) }
-        </tbody>
-    </FullWidthTable>;
+            <tr></tr>
+        </TBody>
+        <TFoot><tr></tr></TFoot>
+    </FullSizeTable>;
 }
