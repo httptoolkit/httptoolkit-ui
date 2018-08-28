8ba76541ec1cdb27d76949abea86977a6fcddfec
Tim Perry <pimterry@gmail.com>
2018-08-28 20:49:20 +0200

    More clearly handle requests/responses without any headers

--- src/app/components/header-details.tsx
+++ src/app/components/header-details.tsx
@@ -2,8 +2,6 @@ import * as _ from 'lodash';
 import * as React from "react";
 import styled, { css } from 'styled-components';
 
-import { CompletedRequest } from "../types";
-
 const Cell = css`
     padding: 0 10px 5px 0;
     vertical-align: top;
@@ -18,15 +16,23 @@ const ValueCell = styled.td`
     ${Cell};
 `;
 
+const EmptyState = styled.div`
+    opacity: 0.5;
+    font-style: italic;
+`;
+
 export const HeaderDetails = (props: { headers: { [key: string]: string }, className?: string }) => {
-    return <table className={props.className}>
-        <tbody>
-            { _.map(props.headers, (value, name) => 
-                <tr key={name}>
-                    <NameCell>{ name }</NameCell>
-                    <ValueCell>{ value }</ValueCell>
-                </tr>
-            ) }
-        </tbody>
-    </table>
+    return _.isEmpty(props.headers) ?
+        <EmptyState>(None)</EmptyState>
+    :
+        <table className={props.className}>
+            <tbody>
+                { _.map(props.headers, (value, name) => 
+                    <tr key={name}>
+                        <NameCell>{ name }</NameCell>
+                        <ValueCell>{ value }</ValueCell>
+                    </tr>
+                ) }
+            </tbody>
+        </table>
 };
