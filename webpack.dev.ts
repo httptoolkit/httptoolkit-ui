62cc4d5590b33d197764d4b9984947f800d9d966
Tim Perry <pimterry@gmail.com>
2018-11-19 14:42:07 +0100

    Ignore js beautify from source map warnings too

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -23,7 +23,8 @@ export = merge(common, {
             exclude: [
                 path.join(__dirname, 'node_modules', 'monaco-editor'),
                 path.join(__dirname, 'node_modules', 'subscriptions-transport-ws'),
-                path.join(__dirname, '..', 'mockttp', 'node_modules', 'subscriptions-transport-ws')
+                path.join(__dirname, '..', 'mockttp', 'node_modules', 'subscriptions-transport-ws'),
+                path.join(__dirname, 'node_modules', 'js-beautify')
             ]
         }
       ]
