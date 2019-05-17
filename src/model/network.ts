b74c38c3bb56fd528f4ca0ef85956842f4409236
Tim Perry <pimterry@gmail.com>
2019-05-17 17:25:23 +0200

    Add logic for parsing & explaining IP addresses

---
+++ src/model/network.ts
@@ -0,0 +1,44 @@
+import * as ipaddr from 'ipaddr.js';
+import { reportError } from '../errors';
+
+function isIPv6(ip: ipaddr.IPv4 | ipaddr.IPv6): ip is ipaddr.IPv6 {
+    return ip.kind() === 'ipv6';
+}
+
+const subnetDescriptionOverrides: _.Dictionary<string> = {
+    'unspecified': 'unknown',
+    'loopback': 'this machine',
+    'private': 'a local network device',
+    'uniqueLocal': 'a local network device',
+    'unicast': '',
+};
+
+// Takes an IPv6 or IPv4 address, and makes it presentable
+export function getReadableIP(ip: string) {
+    let parsedIp: ipaddr.IPv4 | ipaddr.IPv6
+    try {
+        parsedIp = ipaddr.parse(ip);
+    } catch (e) {
+        reportError(`Failed to parse IP: ${ip}`);
+        return ip;
+    }
+
+    if (isIPv6(parsedIp) && parsedIp.isIPv4MappedAddress()) {
+        parsedIp = parsedIp.toIPv4Address();
+    }
+
+    const subnetType = parsedIp.range();
+    const subnetDescription = (
+        subnetType in subnetDescriptionOverrides
+            ? subnetDescriptionOverrides[subnetType]
+            : subnetType
+    ).replace(/([A-Z])/g, ' $1') // camelCase to separate Words
+    .toLowerCase() // Lowercase everything
+    .replace(/^rfc/, 'see RFC ') // Highlight RFCs;
+
+    return parsedIp.toNormalizedString() + (
+        subnetDescription
+            ? ` (${subnetDescription})`
+            : ''
+    );
+}
---
+++ test/unit/model/network.spec.ts
@@ -0,0 +1,39 @@
+import { expect } from "../../test-setup";
+
+import { getReadableIP } from "../../../src/model/network";
+
+describe("IP formatting", () => {
+    it("should return unrecoginized ips untouched", () => {
+        expect(getReadableIP('93.184.216.34')).to.equal('93.184.216.34');
+    });
+
+    it("should mark local network IPv4s", () => {
+        expect(getReadableIP('10.0.0.1')).to.equal('10.0.0.1 (a local network device)');
+    });
+
+    it("should mark IPv4 localhost addresses", () => {
+        expect(getReadableIP('127.1.1.1')).to.equal('127.1.1.1 (this machine)');
+        expect(getReadableIP('127.0.0.1')).to.equal('127.0.0.1 (this machine)');
+    });
+
+    it("should mark IPv6 localhost addresses", () => {
+        expect(getReadableIP('::1')).to.equal('0:0:0:0:0:0:0:1 (this machine)');
+        expect(getReadableIP('::ffff:127.0.0.1')).to.equal('127.0.0.1 (this machine)');
+    });
+
+    it("should return IPv6 mapped address as IPv4", () => {
+        expect(getReadableIP('::ffff:10.0.0.1')).to.equal('10.0.0.1 (a local network device)');
+    });
+
+    it("should return full IPv6 addresses untouched", () => {
+        expect(
+            getReadableIP('fe80:1:2:3:42:abff:fe08:7e77')
+        ).to.equal('fe80:1:2:3:42:abff:fe08:7e77 (link local)');
+    });
+
+    it("should normalize shortened IPv6 addresses", () => {
+        expect(
+            getReadableIP('fc00::7e77')
+        ).to.equal('fc00:0:0:0:0:0:0:7e77 (a local network device)');
+    });
+});
