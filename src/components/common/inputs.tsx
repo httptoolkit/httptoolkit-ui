b5b36b7185f9282a68fb38737320b9f25272605c
Tim Perry <pimterry@gmail.com>
2019-10-14 19:59:41 +0200

    Make input font size explicit
    Previous default size differed between platforms,
    notably Ubuntu seems to use 13.3px, while OSX uses
    11px (so is tiny). This normalizes the two.

--- src/components/common/inputs.tsx
+++ src/components/common/inputs.tsx
@@ -112,6 +112,7 @@ type TextInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 export const TextInput = styled.input.attrs({
     type: 'text'
 })`
+    font-size: 13px;
     padding: 5px 10px;
     border-radius: 4px;
     border: solid 1px ${p => p.theme.containerBorder};
