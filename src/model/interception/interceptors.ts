321ab1fcb5d9bfe03c997eaef16e6fd9cf9b1981
Tim Perry <pimterry@gmail.com>
2020-05-07 12:35:40 +0200

    Add interceptors for Brave & Opera plus Chrome/Chromium/Edge variants

--- src/model/interception/interceptors.ts
+++ src/model/interception/interceptors.ts
@@ -43,9 +43,89 @@ const androidInterceptIconProps = _.assign({
     style: { bottom: '-32px' }
 }, SourceIcons.Android);
 
+const recoloured = (icon: IconProps, color: string) => ({ ...icon, color });
+
 export const MANUAL_INTERCEPT_ID = 'manual-setup';
 
 const INTERCEPT_OPTIONS: _.Dictionary<InterceptorConfig> = {
+    'fresh-chrome': {
+        name: 'Chrome',
+        description: ["Open a preconfigured fresh Chrome window"],
+        iconProps: SourceIcons.Chrome,
+        tags: BROWSER_TAGS
+    },
+    'fresh-chrome-beta': {
+        name: 'Chrome (beta)',
+        description: ["Open a preconfigured fresh Chrome window"],
+        iconProps: recoloured(SourceIcons.Chrome, '#DB4437'),
+        tags: BROWSER_TAGS
+    },
+    'fresh-chrome-dev': {
+        name: 'Chrome (dev)',
+        description: ["Open a preconfigured fresh Chrome window"],
+        iconProps: recoloured(SourceIcons.Chrome, '#74929f'),
+        tags: BROWSER_TAGS
+    },
+    'fresh-chrome-canary': {
+        name: 'Chrome (canary)',
+        description: ["Open a preconfigured fresh Chrome window"],
+        iconProps: recoloured(SourceIcons.Chrome, '#e8ab01'),
+        tags: BROWSER_TAGS
+    },
+    'fresh-chromium': {
+        name: 'Chromium',
+        description: ["Open a preconfigured fresh Chromium window"],
+        iconProps: SourceIcons.Chromium,
+        tags: BROWSER_TAGS
+    },
+    'fresh-chromium-dev': {
+        name: 'Chromium (dev)',
+        description: ["Open a preconfigured fresh Chromium window"],
+        iconProps: recoloured(SourceIcons.Chromium, '#74929f'),
+        tags: BROWSER_TAGS
+    },
+    'fresh-brave': {
+        name: 'Brave',
+        description: ["Open a preconfigured fresh Brave window"],
+        iconProps: SourceIcons.Brave,
+        tags: BROWSER_TAGS
+    },
+    'fresh-firefox': {
+        name: 'Firefox',
+        description: ["Open a preconfigured fresh Firefox window"],
+        iconProps: SourceIcons.Firefox,
+        tags: BROWSER_TAGS
+    },
+    'fresh-safari': {
+        name: 'Safari',
+        description: ["Open a preconfigured fresh Safari window"],
+        iconProps: SourceIcons.Safari,
+        tags: BROWSER_TAGS
+    },
+    'fresh-edge': {
+        name: 'Edge',
+        description: ["Open a preconfigured fresh Edge window"],
+        iconProps: SourceIcons.Edge,
+        tags: BROWSER_TAGS
+    },
+    'fresh-edge-beta': {
+        name: 'Edge (beta)',
+        description: ["Open a preconfigured fresh Edge window"],
+        iconProps: recoloured(SourceIcons.Edge, '#50e6ff'),
+        tags: BROWSER_TAGS
+    },
+    'fresh-edge-canary': {
+        name: 'Edge (canary)',
+        description: ["Open a preconfigured fresh Edge window"],
+        iconProps: recoloured(SourceIcons.Edge, '#ffc225'),
+        tags: BROWSER_TAGS
+    },
+    'fresh-opera': {
+        name: 'Opera',
+        description: ["Open a preconfigured fresh Opera window"],
+        iconProps: SourceIcons.Opera,
+        tags: BROWSER_TAGS
+    },
     'docker-all': {
         name: 'All Docker Containers',
         description: ["Intercept all local Docker traffic"],
@@ -58,30 +138,6 @@ const INTERCEPT_OPTIONS: _.Dictionary<InterceptorConfig> = {
         iconProps: SourceIcons.Docker,
         tags: DOCKER_TAGS
     },
-    'fresh-chrome': {
-        name: 'Fresh Chrome',
-        description: ["Open a preconfigured fresh Chrome window"],
-        iconProps: SourceIcons.Chrome,
-        tags: BROWSER_TAGS
-    },
-    'fresh-firefox': {
-        name: 'Fresh Firefox',
-        description: ["Open a preconfigured fresh Firefox window"],
-        iconProps: SourceIcons.Firefox,
-        tags: BROWSER_TAGS
-    },
-    'fresh-safari': {
-        name: 'Fresh Safari',
-        description: ["Open a preconfigured fresh Safari window"],
-        iconProps: SourceIcons.Safari,
-        tags: BROWSER_TAGS
-    },
-    'fresh-edge': {
-        name: 'Fresh Edge',
-        description: ["Open a preconfigured fresh Edge window"],
-        iconProps: SourceIcons.Edge,
-        tags: BROWSER_TAGS
-    },
     'fresh-terminal': {
         name: 'Fresh Terminal',
         description: ["Open a new terminal preconfigured to intercept all launched processes"],
@@ -91,19 +147,9 @@ const INTERCEPT_OPTIONS: _.Dictionary<InterceptorConfig> = {
     'existing-terminal': {
         name: 'Existing Terminal',
         description: ["Intercept all launched processes from one of your existing terminal windows"],
-        iconProps: _.defaults({ color: '#dd44dd' }, SourceIcons.Terminal),
+        iconProps: recoloured(SourceIcons.Terminal, '#dd44dd'),
         uiConfig: ExistingTerminalCustomUi,
         tags: ['terminal', 'command line', 'cli', 'bash', 'cmd', 'shell', 'php', 'ruby', 'node', 'js']
-    },
-    'electron': {
-        name: 'Electron Application',
-        description: ["Launch an Electron application with all its traffic intercepted"],
-        iconProps: SourceIcons.Electron,
-        uiConfig: ElectronCustomUi,
-        checkRequirements: ({ interceptorVersion }) => {
-            return semver.satisfies(interceptorVersion, "^1.0.1")
-        },
-        tags: ['electron', 'desktop', 'postman']
     },
     'android-device': {
         name: 'Android device',
@@ -126,7 +172,7 @@ const INTERCEPT_OPTIONS: _.Dictionary<InterceptorConfig> = {
             'Intercept any Android device or emulator connected to ADB',
             'Automatically injects system HTTPS certificates into rooted devices & most emulators'
         ],
-        iconProps: _.defaults({ color: '#4285F4' }, androidInterceptIconProps),
+        iconProps: recoloured(androidInterceptIconProps, '#4285F4'),
         checkRequirements: ({ accountStore, serverVersion }) => {
             return semver.satisfies(serverVersion || '', DETAILED_CONFIG_RANGE) &&
                 accountStore.featureFlags.includes("android")
@@ -157,6 +203,16 @@ const INTERCEPT_OPTIONS: _.Dictionary<InterceptorConfig> = {
         description: ["Intercept all HTTP traffic on this machine"],
         iconProps: SourceIcons.Desktop,
         tags: ['local', 'machine', 'system', 'me']
+    },
+    'electron': {
+        name: 'Electron Application',
+        description: ["Launch an Electron application with all its traffic intercepted"],
+        iconProps: SourceIcons.Electron,
+        uiConfig: ElectronCustomUi,
+        checkRequirements: ({ interceptorVersion }) => {
+            return semver.satisfies(interceptorVersion, "^1.0.1")
+        },
+        tags: ['electron', 'desktop', 'postman']
     },
     [MANUAL_INTERCEPT_ID]: {
         name: 'Anything',
