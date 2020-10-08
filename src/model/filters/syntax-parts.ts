af32e8e6545f9c65c6d4e0df98f8360a98fa0d42
Tim Perry <pimterry@gmail.com>
2020-10-08 15:41:05 +0200

    Add number filter syntax matchers

--- src/model/filters/syntax-parts.ts
+++ src/model/filters/syntax-parts.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 
 export type SyntaxMatch = {
     type: 'partial' | 'full';
@@ -10,12 +11,26 @@ export interface Suggestion {
 }
 
 export interface SyntaxPart {
-    // Checks whether the syntax part matches, or _could_ match,
-    // given some text appended to the result
+    /**
+     * Checks whether the syntax part matches, or _could_ match if
+     * some text were appended to the string.
+     *
+     * This will return undefined if the value could not match, e.g.
+     * a number is required and there's a non-number entered already.
+     * If will return a full match if the part is completely present,
+     * and will consume everything it can, and it will return a partial
+     * match if the end of the string was reached without breaking any
+     * rules, but without successfully completing the matcher.
+     */
     match(value: string, index: number): undefined | SyntaxMatch;
 
-    // Returns a list of possible values that would make this
-    // syntax part match, given that it partially matches now.
+    /**
+     * Given that there was a partial match, this returns a list of
+     * possible values that would make this syntax part match fully.
+     *
+     * Don't call it without a partial match, as the behaviour is
+     * undefined.
+     */
     getSuggestions(value: string, index: number): Suggestion[];
 };
 
@@ -49,6 +64,114 @@ export class FixedStringSyntax implements SyntaxPart {
             showAs: this.matcher,
             value: this.matcher
         }];
+    }
+
+}
+
+function isNumberChar(char: string) {
+    const code = char.charCodeAt(0);
+    return code >= 48 && code <= 59; // 0-9 ascii codes
+}
+
+/**
+ * Match a number at this position. Returns the number (as a string)
+ * if one is present here, an empty string if this is the end of the
+ * string, so a number _could_ be appended here, and undefined if
+ * it could not (i.e. a non-number is already present)
+ */
+function getNumberAt(value: string, index: number) {
+    let i: number;
+
+    // Keep reading number chars until we either hit the end of the
+    // string (maybe immediately) or hit a non-number
+    for (i = index; i < value.length; i++) {
+        if (!isNumberChar(value[i])) break;
+    }
+
+    if (i !== index) {
+        // We found at least one number, that's a match:
+        return value.substring(index, i);
+    } else if (i === value.length) {
+        // We were at the end of the string, that's an empty partial match:
+        return "";
+    } else {
+        // We found no characters, and no end of string: fail
+        return undefined;
+    }
+}
+
+export class NumberSyntax implements SyntaxPart {
+
+    match(value: string, index: number): undefined | SyntaxMatch {
+        const matchingNumber = getNumberAt(value, index);
+        if (matchingNumber === undefined) return;
+
+        const consumedChars = matchingNumber.length;
+
+        // Any number is a full match, any empty space is a potential number
+        return {
+            type: (consumedChars > 0)
+                ? 'full'
+                : 'partial',
+            consumed: consumedChars
+        };
+    }
+
+    getSuggestions(value: string, index: number): Suggestion[] {
+        const matchingNumber = getNumberAt(value, index);
+
+        if (!matchingNumber) {
+            return [{
+                showAs: "<number>",
+                value: '0'
+            }];
+        } else {
+            return [{
+                showAs: matchingNumber,
+                value: matchingNumber
+            }];
+        }
+    }
+
+}
+export class FixedLengthNumberSyntax implements SyntaxPart {
+
+    constructor(
+        private requiredLength: number
+    ) {}
+
+    match(value: string, index: number): undefined | SyntaxMatch {
+        const matchingNumber = getNumberAt(value, index);
+        if (matchingNumber === undefined) return;
+
+        const consumedChars = matchingNumber.length;
+
+        if (consumedChars === this.requiredLength) {
+            return { type: 'full', consumed: consumedChars };
+        } else if (consumedChars < this.requiredLength) {
+            return { type: 'partial', consumed: consumedChars };
+        } else {
+            return undefined; // Too many numbers - not a match
+        }
+    }
+
+    getSuggestions(value: string, index: number): Suggestion[] {
+        const matchingNumber = getNumberAt(value, index);
+
+        if (!matchingNumber) {
+            return [{
+                showAs: `<${this.requiredLength}-digit number>`,
+                value: _.repeat("0", this.requiredLength)
+            }];
+        } else {
+            const extendedNumber = matchingNumber +
+                _.repeat("0", this.requiredLength - matchingNumber.length);
+
+            return [{
+                showAs: extendedNumber,
+                value: extendedNumber
+            }];
+        }
     }
 
 }
--- test/unit/model/filters/syntax-parts.spec.ts
+++ test/unit/model/filters/syntax-parts.spec.ts
@@ -1,6 +1,10 @@
 import { expect } from '../../../test-setup';
 
-import { FixedStringSyntax } from "../../../../src/model/filters/syntax-parts";
+import {
+    FixedStringSyntax,
+    NumberSyntax,
+    FixedLengthNumberSyntax
+} from "../../../../src/model/filters/syntax-parts";
 
 describe("String syntax", () => {
 
@@ -57,10 +61,29 @@ describe("String syntax", () => {
         expect(match.consumed).to.equal(8);
     });
 
+    it("should partially match at the end of a string", () => {
+        const part = new FixedStringSyntax("a-string");
+
+        const match = part.match("prefix", 6)!;
+
+        expect(match.type).to.equal('partial');
+        expect(match.consumed).to.equal(0);
+    });
+
     it("should suggest completing the string", () => {
         const part = new FixedStringSyntax("a-string");
 
         const suggestions = part.getSuggestions("a-str", 0)!;
+
+        expect(suggestions.length).to.equal(1);
+        expect(suggestions[0].showAs).to.equal('a-string');
+        expect(suggestions[0].value).to.equal('a-string');
+    });
+
+    it("should suggest completing a completed string", () => {
+        const part = new FixedStringSyntax("a-string");
+
+        const suggestions = part.getSuggestions("a-string", 0)!;
 
         expect(suggestions.length).to.equal(1);
         expect(suggestions[0].showAs).to.equal('a-string');
@@ -75,6 +98,156 @@ describe("String syntax", () => {
         expect(suggestions.length).to.equal(1);
         expect(suggestions[0].showAs).to.equal('a-string');
         expect(suggestions[0].value).to.equal('a-string');
+    });
+
+    it("should suggest completion at the end of a string", () => {
+        const part = new FixedStringSyntax("a-string");
+
+        const suggestions = part.getSuggestions("prefix", 6)!;
+
+        expect(suggestions.length).to.equal(1);
+        expect(suggestions[0].showAs).to.equal('a-string');
+        expect(suggestions[0].value).to.equal('a-string');
+    });
+
+});
+
+describe("Number syntax", () => {
+
+    it("should not match strings", () => {
+        const part = new NumberSyntax();
+        expect(part.match("string", 0)).to.equal(undefined);
+    });
+
+    it("should fully match numbers", () => {
+        const part = new NumberSyntax();
+
+        const match = part.match("12345", 0)!;
+        expect(match.type).to.equal('full');
+        expect(match.consumed).to.equal(5);
+    });
+
+    it("should not match numbers with prefixes", () => {
+        const part = new NumberSyntax();
+
+        const match = part.match("string-123", 0)!;
+        expect(match).to.equal(undefined);
+    });
+
+    it("should fully match numbers with suffixes", () => {
+        const part = new NumberSyntax();
+
+        const match = part.match("123-string", 0)!;
+        expect(match.type).to.equal('full');
+        expect(match.consumed).to.equal(3);
+    });
+
+    it("should fully match numbers with prefixes before the index", () => {
+        const part = new NumberSyntax();
+
+        const match = part.match("string-123", 7)!;
+
+        expect(match.type).to.equal('full');
+        expect(match.consumed).to.equal(3);
+    });
+
+    it("should partially match at the end of a string", () => {
+        const part = new NumberSyntax();
+
+        const match = part.match("string-", 7)!;
+
+        expect(match.type).to.equal('partial');
+        expect(match.consumed).to.equal(0);
+    });
+
+    it("should suggest inserting a number", () => {
+        const part = new NumberSyntax();
+
+        const suggestions = part.getSuggestions("value=", 6)!;
+
+        expect(suggestions.length).to.equal(1);
+        expect(suggestions[0].showAs).to.equal('<number>');
+        expect(suggestions[0].value).to.equal('0');
+    });
+
+    it("should suggest completing an existing number", () => {
+        const part = new NumberSyntax();
+
+        const suggestions = part.getSuggestions("value=123", 6)!;
+
+        expect(suggestions.length).to.equal(1);
+        expect(suggestions[0].showAs).to.equal('123');
+        expect(suggestions[0].value).to.equal('123');
+    });
+
+});
+
+describe("Fixed-length number syntax", () => {
+
+    it("should not match strings", () => {
+        const part = new FixedLengthNumberSyntax(3);
+        expect(part.match("str", 0)).to.equal(undefined);
+    });
+
+    it("should fully match numbers of the correct length", () => {
+        const part = new FixedLengthNumberSyntax(3);
+
+        const match = part.match("123", 0)!;
+        expect(match.type).to.equal('full');
+        expect(match.consumed).to.equal(3);
+    });
+
+    it("should partially match too-short numbers", () => {
+        const part = new FixedLengthNumberSyntax(3);
+
+        const match = part.match("12", 0)!;
+        expect(match.type).to.equal('partial');
+        expect(match.consumed).to.equal(2);
+    });
+
+    it("should not match too-long numbers", () => {
+        const part = new FixedLengthNumberSyntax(3);
+
+        const match = part.match("1234", 0)!;
+        expect(match).to.equal(undefined);
+    });
+
+    it("should fully match numbers with prefixes before the index", () => {
+        const part = new FixedLengthNumberSyntax(3);
+
+        const match = part.match("string-123", 7)!;
+
+        expect(match.type).to.equal('full');
+        expect(match.consumed).to.equal(3);
+    });
+
+    it("should partially match at the end of a string", () => {
+        const part = new FixedLengthNumberSyntax(3);
+
+        const match = part.match("string-", 7)!;
+
+        expect(match.type).to.equal('partial');
+        expect(match.consumed).to.equal(0);
+    });
+
+    it("should suggest inserting a number of the right length", () => {
+        const part = new FixedLengthNumberSyntax(3);
+
+        const suggestions = part.getSuggestions("value=", 6)!;
+
+        expect(suggestions.length).to.equal(1);
+        expect(suggestions[0].showAs).to.equal('<3-digit number>');
+        expect(suggestions[0].value).to.equal('000');
+    });
+
+    it("should suggest extending a number to the right length", () => {
+        const part = new FixedLengthNumberSyntax(3);
+
+        const suggestions = part.getSuggestions("value=50", 6)!;
+
+        expect(suggestions.length).to.equal(1);
+        expect(suggestions[0].showAs).to.equal('500');
+        expect(suggestions[0].value).to.equal('500');
     });
 
 });
