afc0e058fc8c8547e3320993d5816aaf3cc0e3fc
Tim Perry <pimterry@gmail.com>
2020-10-23 12:37:18 +0200

    Warm up the decoding tests, to make them more reliable

--- test/unit/workers/worker-decoding.spec.ts
+++ test/unit/workers/worker-decoding.spec.ts
@@ -4,7 +4,17 @@ import { expect } from '../../test-setup';
 
 import { decodeBody } from '../../../src/services/ui-worker-api';
 
-describe('Worker decoding', () => {
+describe('Worker decoding', function () {
+    this.timeout(500); // Decoding this should be pretty quick - let's enforce that
+
+    before(async function () {
+        this.timeout(10000);
+        // First worker request can be slow seemingly though (~2s), not sure why, might be a
+        // karma issue? Not noticeable in real use, and subsequent calls seem to be very
+        // quick (~1ms) so it's not an issue in practice.
+        await decodeBody(Buffer.from(zlib.gzipSync('Warmup content')), ['gzip']);
+    });
+
     it('should decode a response with no encoding', async () => {
         const body = Buffer.from('hello world');
         const { decoded, encoded } = await decodeBody(body, []);
