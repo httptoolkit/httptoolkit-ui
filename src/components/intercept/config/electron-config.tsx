7effd80ed7c6622978a1c1ff34d9bd25a3e43e5c
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-06-29 11:41:26 +0200

    Tweak help text on macOS
    To take into account that on macOS on the desktop Electron executables
    can be selected again in the file picker, which means their path is not
    really “entered”.

--- src/components/intercept/config/electron-config.tsx
+++ src/components/intercept/config/electron-config.tsx
@@ -194,9 +194,9 @@ class ElectronConfig extends React.Component<{
             </p>
             {
                 platform === 'mac' && previousElectronAppPaths.length < 2 && <p>
-                    For .app bundles, enter either the bundle name (with or without .app)
-                    or the full path to the executable itself, typically stored in
-                    Contents/MacOS inside the bundle.
+                    For .app bundles, you can intercept either the bundle
+                    (the .app directory) or the executable itself,
+                    typically stored in Contents/MacOS inside the bundle.
                 </p>
             }
             <p>
