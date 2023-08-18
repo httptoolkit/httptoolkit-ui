c71468dc7799e64efa1fa8411937093d1f65b2e1
Tim Perry <pimterry@gmail.com>
2023-08-18 14:44:54 +0200

    Separate error reporting by anonymous sessions

--- src/errors.ts
+++ src/errors.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/browser';
+import * as uuid from 'uuid/v4';
 
 import { UI_VERSION, serverVersion, desktopVersion } from './services/service-versions';
 import { ApiError } from './services/server-api-types';
@@ -12,49 +13,63 @@ export function isSentryInitialized() {
 export { Sentry };
 
 export function initSentry(dsn: string | undefined) {
-    if (dsn) {
-        Sentry.init({
-            dsn: dsn,
-            release: UI_VERSION,
-            ignoreErrors: [
-                'ResizeObserver loop limit exceeded', // No visible effect: https://stackoverflow.com/a/50387233/68051
-            ],
-            beforeSend: function (event, hint) {
-                const exception = hint?.originalException;
-                if (exception instanceof ApiError) {
-                    event.fingerprint = [
-                        "{{ default }}",
-                        exception.operationName,
-                        ...(exception.errorCode
-                            ? [exception.errorCode.toString()]
-                            : []
-                        )
-                    ];
-                }
-                return event;
+    if (!dsn) return;
+
+    Sentry.init({
+        dsn: dsn,
+        release: UI_VERSION,
+        ignoreErrors: [
+            'ResizeObserver loop limit exceeded', // No visible effect: https://stackoverflow.com/a/50387233/68051
+        ],
+        beforeSend: function (event, hint) {
+            const exception = hint?.originalException;
+            if (exception instanceof ApiError) {
+                event.fingerprint = [
+                    "{{ default }}",
+                    exception.operationName,
+                    ...(exception.errorCode
+                        ? [exception.errorCode.toString()]
+                        : []
+                    )
+                ];
             }
-        });
-        sentryInitialized = true;
-
-        serverVersion.then((version) => addErrorTag('version:server', version));
-        desktopVersion.then((version) => addErrorTag('version:desktop', version));
-
-        // If we're running in the main window (not the SW),
-        // stop reporting errors after the page starts unloading
-        if (typeof window !== 'undefined') {
-            window.addEventListener('beforeunload', () => {
-                Sentry.getCurrentHub().getClient().getOptions().enabled = false;
-                sentryInitialized = false;
-            });
+            return event;
         }
+    });
+    sentryInitialized = true;
+
+    serverVersion.then((version) => addErrorTag('version:server', version));
+    desktopVersion.then((version) => addErrorTag('version:desktop', version));
+
+    // If we're running in the main window (not the SW),
+    // stop reporting errors after the page starts unloading
+    if (typeof window !== 'undefined') {
+        window.addEventListener('beforeunload', () => {
+            Sentry.getCurrentHub().getClient().getOptions().enabled = false;
+            sentryInitialized = false;
+        });
     }
+
+    Sentry.configureScope((scope) => {
+        // We use a random id to distinguish between many errors in one session vs
+        // one error in many sessions. This isn't persisted and can't be used to
+        // identify anybody between sessions.
+        const randomId = uuid();
+        scope.setUser({
+            id: randomId,
+            username: `anon-${randomId}`
+        });
+    });
 }
 
 export function logErrorsAsUser(email: string | undefined) {
     if (!sentryInitialized) return;
 
     Sentry.configureScope((scope) => {
-        scope.setUser({ email: email });
+        scope.setUser({
+            id: email,
+            email: email
+        });
     });
 }
 
