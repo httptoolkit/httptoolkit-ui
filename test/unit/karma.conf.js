23841f48d7e1a6616daa53aac81e7b146a5632ae
Tim Perry <pimterry@gmail.com>
2024-01-22 20:40:31 +0100

    Make unit tests run under WP5

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -1,16 +1,22 @@
-const path = require('path');
-const tmp = require('tmp');
-tmp.setGracefulCleanup();
+const webpackConfig = require('../../automation/webpack.unittest').default;
 
 module.exports = function(config) {
     config.set({
-        frameworks: ['mocha', 'chai'],
+        frameworks: ['mocha', 'chai', 'webpack'],
+
         files: [
             './**/*.spec.ts',
-            './**/*.spec.tsx'
+            './**/*.spec.tsx',
+
+            {
+                // Required due to https://github.com/codymikol/karma-webpack/issues/450
+                pattern: `${webpackConfig.output.path}/**/*`,
+                watched: false,
+                included: false
+            }
         ],
         mime: { 'text/x-typescript': ['ts', 'tsx'] },
-        webpack: require('../../automation/webpack.unittest').default,
+        webpack: webpackConfig,
         webpackMiddleware: {
             stats: 'errors-only'
         },
