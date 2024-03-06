8e68378b1d893f6105a9a2bf2fcbb4f02158d670
Tim Perry <pimterry@gmail.com>
2024-03-06 19:18:35 +0100

    Fix expanded websocket UI bug with non-accepted connections
    If you had an expanded websocket messages UI, and then you selected a
    websocket event that wasn't accepted (either pending or rejected) then
    the expansion broke - it couldn't show anything, but the logic to
    unexpand didn't activate. The UI now unexpands in this scenario, which
    should avoid that.

--- src/components/view/view-page.tsx
+++ src/components/view/view-page.tsx
@@ -235,7 +235,7 @@ class ViewPage extends React.Component<ViewPageProps> {
                 ) ||
                 (
                     expandedViewCard === 'webSocketMessages' &&
-                    !selectedEvent.isWebSocket()
+                    !(selectedEvent.isWebSocket() && selectedEvent.wasAccepted())
                 )
             ) {
                 runInAction(() => {
